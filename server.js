139177ffee3a0535db2d6cb5370a34bc4055cea1
Pete Bennett <petebennett1982@gmail.com>
2018-10-31 12:27:18 +0000

    Removed old comments

--- server.js
+++ server.js
@@ -1,10 +1,7 @@
 var webpack = require('webpack');
 var WebpackDevServer = require('webpack-dev-server');
 var config = require('./webpack.config');
-// var urlBase = '/ohoc/'
 var urlBase = '/'
-
-// var targetUrl = "http://iporalhistory.co.uk"
 var targetUrl = "http://localhost:6541"
 
 var listeningPort = 7531
@@ -15,27 +12,16 @@ new WebpackDevServer(webpack(config), {
   disableHostCheck: true, 
   historyApiFallback: true,
   proxy: {
-    // [urlBase + 'images']: {
-    //   target: targetUrl,
-    //   // pathRewrite: {'^/api' : ''}
-    // },
     [urlBase + 'api']: {
       target: targetUrl,
-      // pathRewrite: {'^/api' : ''}
     },
-    // [urlBase + 'upload']: {
-    //   target: targetUrl,
-    //   // pathRewrite: {'^/api' : ''}
-    // },
     [urlBase + 'multimedia']: {
       target: targetUrl,
-      // pathRewrite: {'^/api' : ''}
     }
   }
 }).listen(listeningPort, 'localhost', function (err, result) {
   if (err) {
     return console.log(err);
   }
-
   console.log('Listening at http://localhost:'+listeningPort+'/');
 });
